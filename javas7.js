e339c809e8e8cf242721605f5cbe43127597d84e
NachoAv <85850844+NachoAv@users.noreply.github.com>
2022-06-16 21:37:10 -0300

    Add files via upload

---
+++ javas7.js
@@ -0,0 +1,7 @@
+var nombre = prompt('¿Cual es tu nombre? ')
+alert('Gracias por Elegirnos '
+    + nombre)
+var info1 = prompt('¿Ya sos cliente? ')
+var info2 = prompt('¿Tenes muebles de melamina ? ')
+var info3 = prompt('¿Como nos conociste?')
+var info4 = prompt('¿ Necesitas un mueble a medida ? ')
